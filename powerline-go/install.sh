e75f3d76facc32bdbe1b5360b158d45e59a76290
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-23 14:33:46 +0200

    Added theme to powerline-go

--- powerline-go/install.sh
+++ powerline-go/install.sh
@@ -3,8 +3,23 @@
 set -o errexit
 
 : "${TARGET:=/usr/local}"
+ROOT=$(dirname $(readlink -f "$0"))
 
 curl --silent https://api.github.com/repos/justjanne/powerline-go/releases/latest | \
     jq --raw-output '.assets[] | select(.name | endswith("-linux-amd64")) | .browser_download_url' | \
     xargs sudo curl --location --fail --output ${TARGET}/bin/powerline-go
 sudo chmod +x ${TARGET}/bin/powerline-go
+
+mkdir -p ~/.local/etc
+cp ${ROOT}/theme.json ~/.local/etc/powerline-go-theme.json
+
+echo
+echo "############################################################"
+echo "### Now add something like the following to your ~/.bashrc:"
+echo "###"
+echo "function _update_ps1() {"
+echo "    PS1=\"\$(${TARGET}/bin/powerline-go -theme \${HOME}/.local/etc/powerline-go-theme.json -error $? -modules exit,user,cwd,git,docker-context,kube,jobs -newline)\""
+echo "}"
+echo "PROMPT_COMMAND=\"_update_ps1; \$PROMPT_COMMAND\""
+echo "############################################################"
+echo
