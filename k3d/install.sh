0239eb9929e14fdbbfdc561891bcbbbbdc3cce63
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-23 14:58:38 +0200

    Updated to k3d v3

--- k3d/install.sh
+++ k3d/install.sh
@@ -4,9 +4,19 @@ set -o errexit
 
 : "${TARGET:=/usr/local}"
 
-curl --silent https://api.github.com/repos/rancher/k3d/releases/latest | \
+if [[ "$(curl --silent https://api.github.com/repos/rancher/k3d/releases/latest | jq --raw-output '.tag_name')" =~ /^v3./ ]]; then
+    echo "ERROR: Version 3 was released. Please correct install.sh"
+    exit 1
+fi
+
+curl --silent https://api.github.com/repos/rancher/k3d/releases | \
+    jq -r '.[] | select(.tag_name | startswith("v3.0.0-rc.")) | .tag_name' | \
+    sort --version-sort --reverse | head -n 1 | \
+    xargs -I{} curl --silent https://api.github.com/repos/rancher/k3d/releases/tags/{} | \
     jq --raw-output '.assets[] | select(.name == "k3d-linux-amd64") | .browser_download_url' | \
     xargs sudo curl --location --fail --output ${TARGET}/bin/k3d
 sudo chmod +x ${TARGET}/bin/k3d
 
-# k3d complete bash (version >= 3)
+sudo mkdir -p ${TARGET}/etc/bash_completion.d
+k3d completion bash | sudo tee ${TARGET}/etc/bash_completion.d/k3d.sh >/dev/null
+sudo ln -sf ${TARGET}/etc/bash_completion.d/k3d.sh /etc/bash_completion.d/
