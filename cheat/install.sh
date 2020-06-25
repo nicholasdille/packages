85dda29136cca666607addae8414b599ff2a408f
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-25 14:47:48 +0200

    Added cheat

---
+++ cheat/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+
+: "${TARGET:=/usr/local}"
+
+curl -s https://api.github.com/repos/cheat/cheat/releases/latest | \
+    jq --raw-output '.assets[] | select(.name == "cheat-linux-amd64.gz") | .browser_download_url' | \
+    xargs curl -Lf | \
+    gunzip | \
+    sudo tee ${TARGET}/bin/cheat >/dev/null
+sudo chmod +x ${TARGET}/bin/cheat
+
+mkdir -p ~/.config/cheat/cheatsheets/personal
+cheat --init > ~/.config/cheat/conf.yml
+git clone https://github.com/cheat/cheatsheets ~/.config/cheat/cheatsheets/community
