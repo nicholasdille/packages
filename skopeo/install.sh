5089e51d2882f2c6087ee2e4a757cfca06c68881
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-25 14:48:32 +0200

    Added skopeo

---
+++ skopeo/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+
+clean() {
+    docker rm skopeo
+}
+
+trap clean EXIT
+
+: "${TARGET:=/usr/local}"
+
+docker run -i --name skopeo golang bash <<EOF
+git clone --depth 1 https://github.com/containers/skopeo
+cd skopeo
+make bin/skopeo DISABLE_CGO=1
+cp bin/skopeo /
+EOF
+docker cp skopeo:/skopeo - | sudo tar -xvC ${TARGET}/bin/
