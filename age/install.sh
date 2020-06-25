abdd7e71ad8574dccd85c5f969515441648c9203
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-25 14:47:40 +0200

    Added age

---
+++ age/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+
+clean() {
+    docker rm age
+}
+
+trap clean EXIT
+
+: "${TARGET:=/usr/local}"
+
+docker run -i --name age golang bash <<EOF
+git clone https://filippo.io/age
+cd age
+go build -o . filippo.io/age/cmd/...
+cp age age-keygen /
+EOF
+docker cp age:/age - | sudo tar -xvC ${TARGET}/bin/
+docker cp age:/age-keygen - | sudo tar -xvC ${TARGET}/bin/
