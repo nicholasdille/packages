08f527aa2b863db7eaa819ee9b6e98c7a199691c
Nicholas Dille <nicholas.dille@mailbox.org>
2020-06-25 14:49:17 +0200

    Containerized installation

--- flarectl/install.sh
+++ flarectl/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-set -o errexit
+clean() {
+    docker rm flarectl
+}
 
+trap clean EXIT
+
+: "${TARGET:=/usr/local}"
+
+docker run -i --name flarectl golang bash <<EOF
 go get -u github.com/cloudflare/cloudflare-go/cmd/flarectl
+cp /go/bin/flarectl /
+EOF
+docker cp flarectl:/flarectl - | sudo tar -xvC ${TARGET}/bin/
--- yaml-patch/install.sh
+++ yaml-patch/install.sh
@@ -2,6 +2,16 @@
 
 set -o errexit
 
+clean() {
+    docker rm yamlpatch
+}
+
+trap clean EXIT
+
 : "${TARGET:=/usr/local}"
 
+docker run -i --name yamlpatch golang bash <<EOF
 go get github.com/krishicks/yaml-patch/cmd/yaml-patch
+cp /go/bin/yaml-patch /
+EOF
+docker cp yamlpatch:/yaml-patch - | sudo tar -xvC ${TARGET}/bin/
