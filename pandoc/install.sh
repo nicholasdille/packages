45e03b1394f41797baae89a23ff1a533c1936041
Nicholas Dille <nicholas.dille@mailbox.org>
2020-07-03 22:43:14 +0200

    Added pandoc

---
+++ pandoc/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+
+: "${TARGET:=/usr/local}"
+
+curl --silent https://api.github.com/repos/jgm/pandoc/releases/latest | \
+    jq --raw-output '.assets[] | select(.name | endswith("-linux-amd64.tar.gz")) | .browser_download_url' | \
+    xargs curl --location --fail | \
+    sudo tar -xzC ${TARGET} --strip-components=1
