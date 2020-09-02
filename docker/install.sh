bc639b3a35446b08d1b336854dfb25522c2a9184
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-03 00:09:19 +0200

    Fixed shell completion

--- docker/install.sh
+++ docker/install.sh
@@ -5,13 +5,13 @@ set -o errexit
 : "${TARGET:=/usr/local}"
 
 curl --silent https://api.github.com/repos/docker/docker-ce/releases/latest | \
-    jq --raw-output '.tag_name' | \
+    jq --raw-output '.name' | \
     xargs -I{} curl --location --fail https://download.docker.com/linux/static/stable/x86_64/docker-{}.tgz | \
     sudo tar -xzC ${TARGET}/bin/ --strip-components=1
 
 sudo mkdir -p ${TARGET}/etc/bash_completion.d
 curl --silent https://api.github.com/repos/docker/docker-ce/releases/latest | \
     jq --raw-output '.tag_name' | \
-    xargs -I{} curl --location --fail https://github.com/docker/cli/blob/{}/contrib/completion/bash/docker | \
+    xargs -I{} curl --location --fail https://raw.githubusercontent.com/docker/cli/{}/contrib/completion/bash/docker | \
     sudo tee ${TARGET}/etc/bash_completion.d/docker.sh >/dev/null
 sudo ln -sf ${TARGET}/etc/bash_completion.d/docker.sh /etc/bash_completion.d/
