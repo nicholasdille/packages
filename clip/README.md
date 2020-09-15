019ba42add00efb68e153af85056ddab3132ec8e
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-15 14:06:21 +0200

    Added clip

---
+++ clip/README.md
@@ -0,0 +1,7 @@
+# clip
+
+Docker Client Plugins Manager
+
+[GitHub](https://github.com/lukaszlach/clip)
+
+[Docker CLI plugins proposal](https://github.com/docker/cli/issues/1534)
---
+++ clip/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+
+: "${TARGET:=/usr/local}"
+: "${DOCKER_CLI_DIR:=${TARGET}/lib/docker/cli-plugins}"
+
+REQUIRED_DOCKER_VERSION=19.03
+# Get smaller version of required and current version
+DOCKER_VERSION=$(\
+    (\
+        docker version --format '{{.Client.Version}}'; \
+        echo "${REQUIRED_DOCKER_VERSION}"; \
+    ) | \
+    sort --version-sort | \
+    head -n 1; \
+)
+# if smaller version if required version
+if test "${DOCKER_VERSION}" != "${REQUIRED_DOCKER_VERSION}"; then
+    echo "Error: Please install Docker >=${REQUIRED_DOCKER_VERSION}."
+    exit 1
+fi
+
+sudo mkdir --parents ${DOCKER_CLI_DIR}
+
+sudo curl --location --fail --output ${DOCKER_CLI_DIR}/docker-clip https://github.com/lukaszlach/clip/raw/master/docker-clip
+sudo chmod +x ${DOCKER_CLI_DIR}/docker-clip
