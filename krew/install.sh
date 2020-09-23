0b9cca17eda7110203fb28210cf9cb36b44496b7
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-23 12:09:51 +0200

    Added kubectl use

--- krew/install.sh
+++ krew/install.sh
@@ -10,6 +10,7 @@ curl --silent https://api.github.com/repos/kubernetes-sigs/krew/releases/latest 
     sudo tar -xzC ${TARGET}/bin/ ./krew-linux_amd64
 sudo mv ${TARGET}/bin/krew-linux_amd64 ${TARGET}/bin/krew
 krew update
+krew index add kvaps https://github.com/kvaps/krew-index
 krew install \
     access-matrix \
     advise-psp \
@@ -35,4 +36,9 @@ krew install \
     tree \
     view-allocations \
     who-can \
-    whoami
+    whoami \
+    kvaps/use
+
+kubectl use -completion | \
+    sudo tee ${TARGET}/etc/bash_completion.d/kubectl-use.sh >/dev/null
+sudo ln -sf ${TARGET}/etc/bash_completion.d/kubectl-use.sh /etc/bash_completion.d/
