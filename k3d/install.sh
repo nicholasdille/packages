ef1f8f123270d02e3d45686b3031d80440c4fae1
Nicholas Dille <nicholas.dille@mailbox.org>
2020-09-24 16:00:02 +0200

    Fixed k3d

--- k3d/install.sh
+++ k3d/install.sh
@@ -4,15 +4,7 @@ set -o errexit
 
 : "${TARGET:=/usr/local}"
 
-if [[ "$(curl --silent https://api.github.com/repos/rancher/k3d/releases/latest | jq --raw-output '.tag_name')" =~ /^v3./ ]]; then
-    echo "ERROR: Version 3 was released. Please correct install.sh"
-    exit 1
-fi
-
-curl --silent https://api.github.com/repos/rancher/k3d/releases | \
-    jq -r '.[] | select(.tag_name | startswith("v3.0.0-rc.")) | .tag_name' | \
-    sort --version-sort --reverse | head -n 1 | \
-    xargs -I{} curl --silent https://api.github.com/repos/rancher/k3d/releases/tags/{} | \
+curl --silent https://api.github.com/repos/rancher/k3d/releases/latest | \
     jq --raw-output '.assets[] | select(.name == "k3d-linux-amd64") | .browser_download_url' | \
     xargs sudo curl --location --fail --output ${TARGET}/bin/k3d
 sudo chmod +x ${TARGET}/bin/k3d
