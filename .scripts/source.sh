367405252188ada586255c8958a0ccddd5d70954
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-06 15:49:25 +0200

    Externalized variabled

--- .scripts/source.sh
+++ .scripts/source.sh
@@ -1,18 +1,26 @@
-BASE_DIR=$(dirname $(readlink -f "$0"))
+if test "$0" == "bash"; then
+    >&2 echo "ERROR: Do not source this file."
+    exit 1
+fi
+
+SCRIPT_BASE_DIR=$(dirname $(readlink -f "$0"))
 
 : "${SOURCE_LOCAL_FILES:=false}"
 
-: "${TARGET:=/usr/local}"
-
-: "${CURL_DOWNLOAD_SILENT:=false}"
-: "${TAR_VERBOSE:=false}"
+>&2 echo "Sourcing variables..."
+if ${SOURCE_LOCAL_FILES}; then
+    >&2 echo "  !!! FROM LOCAL FILE !!!"
+    source "${SCRIPT_BASE_DIR}/../.scripts/variables.sh"
+else
+    source <(curl --silent --location --fail https://pkg.dille.io/.scripts/variables.sh)
+fi
 
 for file in linux github; do
     >&2 echo "Sourcing ${file}..."
 
     if ${SOURCE_LOCAL_FILES}; then
         >&2 echo "  !!! FROM LOCAL FILE !!!"
-        source "${BASE_DIR}/../.scripts/${file}.sh"
+        source "${SCRIPT_BASE_DIR}/../.scripts/${file}.sh"
     else
         source <(curl --silent --location --fail https://pkg.dille.io/.scripts/${file}.sh)
     fi
---
+++ .scripts/variables.sh
@@ -0,0 +1,6 @@
+: "${TARGET_BASE:=/usr/local}"
+: "${TARGET_BIN:=${TARGET_BASE}/bin}"
+: "${TARGET_COMPLETION:=${TARGET_BASE}/etc/bash_completion.d}"
+
+: "${CURL_DOWNLOAD_SILENT:=false}"
+: "${TAR_VERBOSE:=false}"
