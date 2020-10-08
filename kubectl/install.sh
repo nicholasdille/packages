f2d648aad9eb043b0b5b0cd31bc32708552ffc00
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-08 15:57:52 +0200

    Updated to use official URL

--- kubectl/install.sh
+++ kubectl/install.sh
@@ -4,13 +4,10 @@ set -o errexit
 
 : "${TARGET:=/usr/local}"
 
-curl --silent https://storage.googleapis.com/kubernetes-release/release/stable.txt | \
+curl --silent https://dl.k8s.io/release/stable.txt | \
     xargs -I{} sudo curl --location --output ${TARGET}/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/{}/bin/linux/amd64/kubectl
 sudo chmod +x ${TARGET}/bin/kubectl
 
 sudo mkdir -p ${TARGET}/etc/bash_completion.d
 kubectl completion bash | sudo tee ${TARGET}/etc/bash_completion.d/kubectl.sh >/dev/null
 sudo ln -sf ${TARGET}/etc/bash_completion.d/kubectl.sh /etc/bash_completion.d/
-
-#alias k=kubectl
-#complete -F __start_kubectl k
