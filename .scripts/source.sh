59631ed7651a3899b55fec3835280847076b4038
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-08 15:54:44 +0200

    Improved output

--- .scripts/source.sh
+++ .scripts/source.sh
@@ -7,21 +7,22 @@ SCRIPT_BASE_DIR=$(dirname $(readlink -f "$0"))
 
 : "${SOURCE_LOCAL_FILES:=false}"
 
->&2 echo "Sourcing variables..."
+>&2 echo -n "Sourcing variables..."
 if ${SOURCE_LOCAL_FILES}; then
-    >&2 echo "  !!! FROM LOCAL FILE !!!"
+    >&2 echo " !!! FROM LOCAL FILE !!!"
     source "${SCRIPT_BASE_DIR}/../.scripts/variables.sh"
 else
+    echo
     source <(curl --silent --location --fail https://pkg.dille.io/.scripts/variables.sh)
 fi
 
 for file in control linux github; do
-    >&2 echo "Sourcing ${file}..."
-
+    >&2 echo -n "Sourcing ${file}..."
     if ${SOURCE_LOCAL_FILES}; then
-        >&2 echo "  !!! FROM LOCAL FILE !!!"
+        >&2 echo " !!! FROM LOCAL FILE !!!"
         source "${SCRIPT_BASE_DIR}/../.scripts/${file}.sh"
     else
+        echo
         source <(curl --silent --location --fail https://pkg.dille.io/.scripts/${file}.sh)
     fi
 done
