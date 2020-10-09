4d0b3b3436c58be7ca7c0f1c3cc9e4c5ad6cecf7
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-09 12:48:42 +0200

    Name not required for type tarball

--- act/install.sh
+++ act/install.sh
@@ -7,7 +7,6 @@ source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh
 unlock_sudo
 
 github_install \
-    --name act \
     --repo nektos/act \
     --match suffix \
     --asset _Linux_x86_64.tar.gz \
