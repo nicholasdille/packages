d638319f46d04fa15cd81012e386679738b1b07f
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-09 09:24:17 +0200

    Tested gunzip support

--- cheat/install.sh
+++ cheat/install.sh
@@ -2,14 +2,16 @@
 
 set -o errexit
 
-: "${TARGET:=/usr/local}"
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
 
-curl --silent https://api.github.com/repos/cheat/cheat/releases/latest | \
-    jq --raw-output '.assets[] | select(.name == "cheat-linux-amd64.gz") | .browser_download_url' | \
-    xargs curl --location --fail | \
-    gunzip | \
-    sudo tee ${TARGET}/bin/cheat >/dev/null
-sudo chmod +x ${TARGET}/bin/cheat
+unlock_sudo
+
+github_install \
+    --repo cheat/cheat \
+    --match name \
+    --asset cheat-linux-amd64.gz \
+    --type gunzip \
+    --name cheat
 
 mkdir -p ~/.config/cheat/cheatsheets/personal
 cheat --init > ~/.config/cheat/conf.yml
