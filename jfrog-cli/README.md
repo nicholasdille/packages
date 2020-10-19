fef9547f07bd387aaa128046f9de50f34e6faad4
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-19 16:13:17 +0200

    Added JFrog CLI

---
+++ jfrog-cli/README.md
@@ -0,0 +1,5 @@
+# jfrog-cli
+
+JFrog CLI is a client that provides a simple interface that automates access to the JFrog products
+
+[GitHub](https://github.com/jfrog/jfrog-cli)
---
+++ jfrog-cli/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+unlock_sudo
+
+TAG=$(
+    github_find_latest_release jfrog/jfrog-cli | \
+        jq --raw-output '.tag_name'
+)
+
+echo "https://api.bintray.com/content/jfrog/jfrog-cli-go/${TAG#v}/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64" | \
+    download_file | \
+    store_file jfrog | \
+    make_executable
