04e049887d1ba1336c35fa41cd211b5c1e282128
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-19 12:28:13 +0200

    Added bat (closes #61)

---
+++ bat/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+unlock_sudo
+
+filename=$(mktemp)
+github_find_latest_release sharkdp/bat | \
+    github_resolve_assets | \
+    github_select_asset_by_suffix -x86_64-unknown-linux-gnu.tar.gz | \
+    github_get_asset_download_url | \
+    download_file \
+    >"${filename}"
+
+sudo tar -xz -f "${filename}" -C "${TARGET_BIN}" --wildcards --strip-components=1 "*/bat"
+sudo tar -xz -f "${filename}" -C "${TARGET_BASE}/man/man1" --wildcards --strip-components=1 "*/bat.1"
+
+rm "${filename}"
