d3ae5ee86e80bc8e7da5968bd862a045bb5ff4be
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-20 21:02:10 +0200

    Added pyenv (#51)

---
+++ pyenv/README.md
@@ -0,0 +1,7 @@
+# pyenv
+
+A beautiful and useful low-latency prompt for your shell, written in go
+
+[GitHub](https://github.com/pyenv/pyenv)
+
+[pyenv-virtualenv](https://github.com/pyenv/pyenv-virtualenv)
---
+++ pyenv/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+unlock_sudo
+
+if ! test -d "${HOME}/.pyenv"; then
+    git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
+fi
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+if ! test -d "$(pyenv root)/plugins/pyenv-virtualenv"; then
+    git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)/plugins/pyenv-virtualenv"
+fi
+
+export DOCKER_BUILDKIT=1
+curl --silent https://pkg.dille.io/gvm/Dockerfile | \
+    docker build --tag pyenv -
+
+echo
+echo "#############################################"
+echo "### Now add the following to your ~/.bashrc:"
+echo "###"
+echo "export PYENV_ROOT=\$HOME/.pyenv"
+echo "export PATH=\$PYENV_ROOT/bin:\$PATH"
+echo "eval \$(pyenv init -)"
+echo "eval \$(pyenv virtualenv-init -)"
+echo "#############################################"
+echo
