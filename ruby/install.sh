6e8c489da899793ea4a5eec104429aa5a6d0e63a
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-21 09:30:26 +0200

    Fixes for ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -21,13 +21,20 @@ TAG=$(
 
 export PATH="${HOME}/.rbenv/bin:${PATH}"
 eval "$(rbenv init -)"
-docker run -it --rm --name rbenv --env HOME --env PATH --volume "${HOME}:${HOME}" --workdir "${HOME}" --user "$(id -u):$(id -g)" rbenv rbenv install "${TAG#v}" --skip-existing
-pyenv rehash
+docker run -it --rm --name rbenv \
+    --env HOME \
+    --env PATH \
+    --volume "${HOME}:${HOME}" \
+    --workdir "${HOME}" \
+    --user "$(id -u):$(id -g)" \
+    rbenv \
+    rbenv install "${TAG#v}" --skip-existing
+rbenv rehash
 
 echo
 echo "#############################################"
 echo "### Now add the following to your ~/.bashrc:"
 echo "###"
-echo "export RBENV_VERSION=${TAG#v}"
+echo "export PATH=\${HOME}/.rbenv/versions/${TAG#v}/bin:\${PATH}"
 echo "#############################################"
 echo
