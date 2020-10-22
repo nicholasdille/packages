9d6ca668ca1e6ba8779457ae4c994d34e6524d19
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-22 12:04:04 +0200

    Added promql-cli (closes #39)

---
+++ promql-cli/README.md
@@ -0,0 +1,5 @@
+# promql
+
+Prometheus Query CLI
+
+[GitHub](https://github.com/nalbury/promql-cli)
---
+++ promql-cli/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+check_docker
+unlock_sudo
+
+TAG=$(
+    github_find_latest_release nalbury/promql-cli | \
+        jq --raw-output '.tag_name'
+)
+
+build_containerized golang <<EOF
+git clone https://github.com/nalbury/promql-cli
+cd promql-cli
+git checkout "${TAG}"
+go mod download
+go build -o promql .
+cp promql /
+EOF
+extract_file_from_container promql
