1688684182a138daf5b67a4fdaa45bf0e2c2847f
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-22 13:02:14 +0200

    Added oci-runtime-tool (closes #35)

---
+++ oci-runtime-tool/README.md
@@ -0,0 +1,5 @@
+# oci-runtime-tool
+
+OCI Runtime Tools
+
+[GitHub](https://github.com/opencontainers/runtime-tools)
---
+++ oci-runtime-tool/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+check_docker
+unlock_sudo
+
+build_containerized golang <<EOF
+go get github.com/cpuguy83/go-md2man
+go get -d github.com/opencontainers/runtime-tools/cmd/oci-runtime-tool
+cd /go/src/github.com/opencontainers/runtime-tools/
+make all man
+cp oci-runtime-tool /
+EOF
+extract_file_from_container oci-runtime-tool
+
+for MAN in oci-runtime-tool oci-runtime-tool-generate oci-runtime-tool-validate; do
+    # shellcheck disable=SC2154
+    docker cp "${container_name}:/go/src/github.com/opencontainers/runtime-tools/${MAN}.1" - | \
+        sudo tee "${TARGET_BASE}/share/man/man1/${MAN}.1" >/dev/null
+done
