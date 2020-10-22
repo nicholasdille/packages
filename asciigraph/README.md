4a1777efb3459e1f7bc4a511d539a1ba850ff9d1
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-22 13:07:26 +0200

    Added asciigraph

---
+++ asciigraph/README.md
@@ -0,0 +1,5 @@
+# asciigraph
+
+Go package to make lightweight ASCII line graph ╭┈╯ in command line apps with no other dependencies
+
+[GitHub](https://github.com/guptarohit/asciigraph)
---
+++ asciigraph/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+# shellcheck source=.scripts/source.sh
+source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
+
+unlock_sudo
+
+github_install \
+    --repo guptarohit/asciigraph \
+    --match suffix \
+    --asset _Linux_x86_64.tar.gz \
+    --type tarball \
+    --include asciigraph
