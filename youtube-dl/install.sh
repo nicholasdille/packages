939847157068be4f786650135cb94b6f9b4c848f
Nicholas Dille <nicholas.dille@mailbox.org>
2020-10-31 21:11:40 +0100

    Updated to use codeberg (closes #113)

--- youtube-dl/install.sh
+++ youtube-dl/install.sh
@@ -5,11 +5,30 @@ set -o errexit
 # shellcheck source=.scripts/source.sh
 source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh)
 
+check_docker
 unlock_sudo
 
-github_find_latest_release ytdl-org/youtube-dl | \
-    jq --raw-output '.tag_name' | \
-    xargs -I{} echo "https://github.com/ytdl-org/youtube-dl/releases/download/{}/youtube-dl" | \
-    download_file | \
-    store_file youtube-dl | \
-    make_executable
+TAG=$(
+    codeberg_get_tags polarisfm/youtube-dl | \
+        codeberg_select_latest_tag
+)
+if test -z "${TAG}"; then
+    echo "ERROR: Unable to determine version to install."
+    exit 1
+fi
+
+build_containerized python <<EOF
+apt-get update
+apt-get -y install zip pandoc
+git clone https://codeberg.org/polarisfm/youtube-dl /tmp/youtube-dl
+cd /tmp/youtube-dl
+git checkout "${TAG}"
+make youtube-dl youtube-dl.1 youtube-dl.bash-completion
+ls -l
+cp youtube-dl youtube-dl.1 youtube-dl.bash-completion /
+EOF
+extract_file_from_container youtube-dl
+docker cp "${container_name}:/youtube-dl.1" - | \
+    sudo tee "${TARGET_BASE}/share/man/man1/youtube-dl.1" >/dev/null
+docker cp "${container_name}:/youtube-dl.bash-completion" - | \
+    store_completion youtube-dl
