ff829c6662b7a568899794371a1c9c787cd12c73
Nicholas Dille <nicholas.dille@mailbox.org>
2020-11-02 11:01:31 +0100

    Fixed installation of jwt-cli

--- jwt-cli/install.sh
+++ jwt-cli/install.sh
@@ -7,9 +7,9 @@ source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh
 
 unlock_sudo
 
-github_find_latest_release mike-engel/jwt-cli | \
-    github_resolve_assets | \
-    github_select_asset_by_suffix -linux.tar.gz | \
-    github_get_asset_download_url | \
-    download_file | \
-    untar_file --strip-components=2 jwt-cli
+github_install \
+    --repo mike-engel/jwt-cli \
+    --match suffix \
+    --asset -linux.tar.gz \
+    --type tarball \
+    --include jwt
