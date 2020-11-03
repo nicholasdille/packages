3d6e7fc22710dbbbd983e4124f4424c8ca392fd1
Nicholas Dille <nicholas.dille@mailbox.org>
2020-11-03 16:55:49 +0100

    Moved pyenv to TARGET_BASE #79

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -8,16 +8,16 @@ source <(curl --silent --location --fail https://pkg.dille.io/.scripts/source.sh
 check_docker
 unlock_sudo
 
-if ! test -d "${HOME}/.pyenv"; then
-    git clone https://github.com/pyenv/pyenv.git "${HOME}/.pyenv"
+if ! test -d "${TARGET_BASE}/pyenv"; then
+    ${SUDO} git clone https://github.com/pyenv/pyenv.git "${TARGET_BASE}/pyenv"
 fi
-export PYENV_ROOT="${HOME}/.pyenv"
+export PYENV_ROOT="${TARGET_BASE}/pyenv"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
-if ! test -d "$(pyenv root)/plugins/pyenv-virtualenv"; then
-    git clone https://github.com/pyenv/pyenv-virtualenv.git "$(pyenv root)/plugins/pyenv-virtualenv"
+if ! test -d "${PYENV_ROOT}/plugins/pyenv-virtualenv"; then
+    ${SUDO} git clone https://github.com/pyenv/pyenv-virtualenv.git "${PYENV_ROOT}/plugins/pyenv-virtualenv"
 fi
-if ! test -d "$(pyenv root)/plugins/pyenv-doctor"; then
-    git clone https://github.com/pyenv/pyenv-doctor.git "$(pyenv root)/plugins/pyenv-doctor"
+if ! test -d "${PYENV_ROOT}/plugins/pyenv-doctor"; then
+    ${SUDO} git clone https://github.com/pyenv/pyenv-doctor.git "${PYENV_ROOT}/plugins/pyenv-doctor"
 fi
 
 export DOCKER_BUILDKIT=1
@@ -29,22 +29,14 @@ docker run --rm --name pyenv \
     --env PYENV \
     --env HOME \
     --env PATH \
-    --volume "${HOME}:${HOME}" \
-    --workdir "${HOME}" \
-    --user "$(id -u):$(id -g)" \
+    --volume "${TARGET_BASE}/pyenv:${TARGET_BASE}/pyenv" \
     pyenv \
     pyenv doctor
 
-echo
-echo "#############################################"
-echo "### Now add the following to your ~/.bashrc:"
-echo "###"
-echo "export PYENV_ROOT=\$HOME/.pyenv"
-echo "export PATH=\$PYENV_ROOT/bin:\$PATH"
-echo "eval \$(pyenv init -)"
-echo "eval \$(pyenv virtualenv-init -)"
-echo "#############################################"
-echo
+curl --silent https://pkg.dille.io/pkg.sh | \
+    bash -s file pyenv profile.d.pyenv.sh | \
+    TARGET_BASE=${TARGET_BASE} envsubst '${TARGET_BASE}' | \
+    store_file pyenv.sh /etc/profile.d
 
 echo
 echo "#################################################"
---
+++ pyenv/profile.d.pyenv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export PYENV_ROOT="${TARGET_BASE}/pyenv"
+export PATH="${PYENV_ROOT}/plugins/pyenv-virtualenv/shims:${PYENV_ROOT}/shims:${PYENV_ROOT}/bin:${PATH}"
