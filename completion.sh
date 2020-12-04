eac7de072173bb214fba17288ac2958fb41b9aff
Nicholas Dille <nicholas.dille@mailbox.org>
2020-12-04 16:29:21 +0100

    Fixed after shellcheck

--- completion.sh
+++ completion.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function _pkgctl() {
     local command=${COMP_WORDS[1]}
     local cur=${COMP_WORDS[COMP_CWORD]}
@@ -14,32 +16,32 @@ function _pkgctl() {
                 *)
                     options=( --force --file )
                     packages="$(jq --raw-output '.packages[].name' packages.json)"
-                    COMPREPLY=( $(compgen -W "${packages} ${options[*]}" -- ${cur}) )
+                    COMPREPLY=( "$(compgen -W "${packages} ${options[*]}" -- "${cur}")" )
                     ;;
             esac
             ;;
         file)
             if test "${prev}" == "file"; then
                 packages="$(jq --raw-output '.packages[] | select(.files != null) | .name' packages.json)"
-                COMPREPLY=( $(compgen -W "${packages}" -- ${cur}) )
+                COMPREPLY=( "$(compgen -W "${packages}" -- "${cur}")" )
 
             elif test "${#COMP_WORDS[@]}" == 4; then
                 files="$(jq --raw-output --arg package "${COMP_WORDS[2]}" '.packages[] | select(.name == $package) | .files[].name' packages.json)"
-                COMPREPLY=( $(compgen -W "${files}" -- ${cur}) )
+                COMPREPLY=( "$(compgen -W "${files}" -- "${cur}")" )
             fi
             ;;
         inspect|version)
             packages="$(jq --raw-output '.packages[].name' packages.json)"
-            COMPREPLY=( $(compgen -W "${packages}" -- ${cur}) )
+            COMPREPLY=( "$(compgen -W "${packages}" -- "${cur}")" )
             ;;
         search)
             options=( --name --desc --tags )
-            COMPREPLY=( $(compgen -W "${options[*]}" -- ${cur}) )
+            COMPREPLY=( "$(compgen -W "${options[*]}" -- "${cur}")" )
             ;;
         *)
             commands=( help cache bootstrap file inspect install list search tags version )
             options=( --version )
-            COMPREPLY=( $(compgen -W "${commands[*]} ${options[*]}" -- ${cur}) )
+            COMPREPLY=( "$(compgen -W "${commands[*]} ${options[*]}" -- "${cur}")" )
             ;;
     esac
 }
