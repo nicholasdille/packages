756ce031bf8b025f839d714f6587e526c4b4c695
Nicholas Dille <nicholas.dille@mailbox.org>
2020-12-10 16:14:11 +0100

    Improved testing

--- test.sh
+++ test.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 set -o errexit
 
-PACKAGE=$1
-if test -z "${PACKAGE}"; then
-    echo "ERROR: Package name not specified"
-    exit 1
-fi
-
 make packages.json
 
-DOCKER_BUILDKIT=1 docker build --target ubuntu --tag test-package:ubuntu .
+source /etc/lsb-release
+if test "${DISTRIB_ID}" != "Ubuntu"; then
+    echo "ERROR: Distribution ${DISTRIB_ID} is unsupported"
+    exit 1
+fi
+DOCKER_BUILDKIT=1 docker build --target ${DISTRIB_ID,,}-${DISTRIB_CODENAME} --tag test-package:ubuntu .
 
-docker ps --filter name="test-${PACKAGE}" --all --quiet | xargs -r docker rm -f
-docker run -d --name "test-${PACKAGE}" --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock test-package:ubuntu
+docker ps --filter name="test-build" --all --quiet | xargs -r docker rm -f
+docker run -d --name "test-build" --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock test-package:ubuntu
 
 function cleanup() {
-    docker rm -f "test-${PACKAGE}"
+    docker rm -f "test-build"
 }
 trap cleanup EXIT
 
-docker exec "test-${PACKAGE}" mkdir /root/.pkgctl
-docker cp pkgctl.sh "test-${PACKAGE}:/usr/local/bin/pkgctl"
-docker cp packages.json "test-${PACKAGE}:/root/.pkgctl/packages.json"
+docker exec "test-build" mkdir /root/.pkgctl
+docker cp pkgctl.sh "test-build:/usr/local/bin/pkgctl"
+docker cp packages.json "test-build:/root/.pkgctl/packages.json"
 
-REQUIRES_DOCKER="$(
-    jq --raw-output --arg package "${PACKAGE}" '.packages[] | select(.name == $package) | .install.docker' packages.json
-)"
-if test "${REQUIRES_DOCKER}" == "true"; then
-    docker exec "test-${PACKAGE}" pkgctl install docker
-fi
+while test "$#" -gt 0; do
+    PACKAGE=$1
+    shift
 
-docker exec "test-${PACKAGE}" pkgctl install ${PACKAGE}
+    REQUIRES_DOCKER="$(
+        jq --raw-output --arg package "${PACKAGE}" '.packages[] | select(.name == $package) | .install.docker' packages.json
+    )"
+    if test "${REQUIRES_DOCKER}" == "true"; then
+        docker exec "test-build" pkgctl install docker
+    fi
+
+    docker exec "test-build" pkgctl install ${PACKAGE}
+
+done
