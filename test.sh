6083609cd1d4c835df43192e3ff591802036a619
Nicholas Dille <nicholas.dille@mailbox.org>
2020-12-17 16:11:36 +0100

    Improved test script

--- test.sh
+++ test.sh
@@ -9,10 +9,20 @@ if test "${DISTRIB_ID}" != "Ubuntu"; then
     echo "ERROR: Distribution ${DISTRIB_ID} is unsupported"
     exit 1
 fi
-DOCKER_BUILDKIT=1 docker build --target "${DISTRIB_ID,,}-${DISTRIB_CODENAME}" --tag test-package:ubuntu .
+DOCKER_BUILDKIT=1 docker build \
+    --target "${DISTRIB_ID,,}-${DISTRIB_CODENAME}" \
+    --tag test-package:ubuntu .
 
-docker ps --filter name="test-build" --all --quiet | xargs -r docker rm -f
-docker run -d --name "test-build" --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock test-package:ubuntu
+docker ps \
+        --filter name="test-build" \
+        --all \
+        --quiet | \
+    xargs -r docker rm --force
+docker run \
+    --name "test-build" \
+    --detach \
+    --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    test-package:ubuntu
 
 function cleanup() {
     docker rm -f "test-build"
@@ -28,12 +38,16 @@ while test "$#" -gt 0; do
     shift
 
     REQUIRES_DOCKER="$(
-        jq --raw-output --arg package "${PACKAGE}" '.packages[] | select(.name == $package) | .install.docker' packages.json
+        jq \
+            --raw-output \
+            --arg package "${PACKAGE}" \
+            '.packages[] | select(.name == $package) | .install.docker' \
+            packages.json
     )"
     if test "${REQUIRES_DOCKER}" == "true"; then
-        docker exec "test-build" pkgctl install docker
+        docker exec --env TARGET_BASE "test-build" pkgctl install docker
     fi
 
-    docker exec "test-build" pkgctl install "${PACKAGE}"
+    docker exec --env TARGET_BASE "test-build" pkgctl install "${PACKAGE}"
 
 done
