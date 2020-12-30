51b307f266d9921f5ad161b0387c7f6462e69400
Nicholas Dille <nicholas.dille@mailbox.org>
2020-12-30 16:51:24 +0100

    Improved testing

--- test.sh
+++ test.sh
@@ -9,29 +9,26 @@ if test "${DISTRIB_ID}" != "Ubuntu"; then
     echo "ERROR: Distribution ${DISTRIB_ID} is unsupported"
     exit 1
 fi
-DOCKER_BUILDKIT=1 docker build \
-    --target "${DISTRIB_ID,,}-${DISTRIB_CODENAME}" \
-    --tag test-package:ubuntu .
 
 docker ps \
         --filter name="test-build" \
         --all \
         --quiet | \
-    xargs -r docker rm --force
+    xargs -r \
+        docker rm --force
 docker run \
     --name "test-build" \
     --detach \
+    --env GITHUB_TOKEN \
     --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-    test-package:ubuntu
+    --mount type=bind,src=${PWD}/packages.json,dst=/root/.pkgctl/packages.json \
+    --mount type=bind,src=${PWD}/pkgctl.sh,dst=/usr/local/bin/pkgctl.sh \
+    "nicholasdille/packages-build:${DISTRIB_ID,,}-${DISTRIB_CODENAME}"
 
 function cleanup() {
     docker rm -f "test-build"
 }
 trap cleanup EXIT
-
-docker exec "test-build" mkdir /root/.pkgctl
-docker cp pkgctl.sh "test-build:/usr/local/bin/pkgctl"
-docker cp packages.json "test-build:/root/.pkgctl/packages.json"
 
 while test "$#" -gt 0; do
     PACKAGE=$1
@@ -45,9 +42,9 @@ while test "$#" -gt 0; do
             packages.json
     )"
     if test "${REQUIRES_DOCKER}" == "true"; then
-        docker exec --env TARGET_BASE "test-build" pkgctl install docker
+        docker exec --env TARGET_BASE "test-build" pkgctl.sh install docker
     fi
 
-    docker exec --env TARGET_BASE "test-build" pkgctl install "${PACKAGE}"
+    docker exec --env TARGET_BASE "test-build" pkgctl.sh install "${PACKAGE}"
 
 done
