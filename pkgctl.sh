e7466fc75fea58e59cda7baabb382e141d31fe72
Nicholas Dille <nicholas.dille@mailbox.org>
2021-01-04 13:00:30 +0100

    Closes #782

--- pkgctl.sh
+++ pkgctl.sh
@@ -548,35 +548,6 @@ function add_prefix() {
     done
 }
 
-function run_filters() {
-    local filters=("$@")
-
-    # slurp input
-    data=$(cat | jq --slurp --raw-input --raw-output --compact-output --monochrome-output .)
-
-    for i in ${!filters[*]}; do
-        >&2 echo "Applying filter $i <${filters[$i]}>..."
-
-        # apply filter and store output
-        data=$(echo -n "${data}" | eval "${filters[$i]}")
-    done
-    echo "${data}"
-}
-
-function run_tasks() {
-    local tasks=("$@")
-
-    # slurp input
-    data=$(cat | jq --slurp --raw-input --raw-output --compact-output --monochrome-output .)
-
-    >&2 echo
-    for i in ${!tasks[*]}; do
-        >&2 echo "Running task $i"
-        echo -n "${data}" | eval "${tasks[$i]}"
-        >&2 echo
-    done
-}
-
 function github_api() {
     local path=$1
 
