75fafc35872a082c9dc3829da2602f73ab86c6e6
Nicholas Dille <nicholas.dille@mailbox.org>
2021-01-12 14:08:26 +0100

    Removed superceded functions for #738

--- pkgctl.sh
+++ pkgctl.sh
@@ -38,6 +38,7 @@ function remove_temporary_directory() {
         rm -rf "${temporary_directory:?}"
     fi
 }
+
 function check_target() {
     >&2 echo "Checking existence of target directory <${TARGET_BASE}>"
     if ! test -d "${TARGET_BASE}"; then
@@ -91,16 +92,6 @@ function unlock_sudo() {
     create_target
 }
 
-function download_file() {
-    if ${CURL_DOWNLOAD_SILENT}; then
-        CURL_ADDITIONAL_PARAMS="--silent"
-    fi
-
-    >&2 echo "Downloading file..."
-    cat | \
-        xargs curl --location --fail ${CURL_ADDITIONAL_PARAMS}
-}
-
 function download() {
     local url=$1
     if test -z "${url}"; then
@@ -109,19 +100,6 @@ function download() {
     fi
     >&2 echo "Downloading file from <${url}>..."
     curl --location --fail --remote-name "${url}"
-}
-
-function untar_file() {
-    local parameters=("$@")
-
-    if ${TAR_VERBOSE}; then
-        TAR_ADDITIONAL_PARAMS="-v"
-    fi
-
-    >&2 echo "Unpacking asset to directory ${TARGET_BIN}..."
-    >&2 echo "  Including parameters <${parameters[*]}>"
-    cat | \
-        ${SUDO} tar -x -z ${TAR_ADDITIONAL_PARAMS} -C "${TARGET_BIN}" "${parameters[@]}"
 }
 
 function unpack() {
@@ -174,56 +152,6 @@ function ungz() {
     gunzip "${temporary_directory}/${filename}"
 }
 
-function unxz_file() {
-    local parameters=("$@")
-
-    if ${TAR_VERBOSE}; then
-        TAR_ADDITIONAL_PARAMS="-v"
-    fi
-
-    >&2 echo "Unpacking asset to directory ${TARGET_BIN}..."
-    >&2 echo "  Including parameters <${parameters[*]}>"
-    cat | \
-        ${SUDO} tar -x -J ${TAR_ADDITIONAL_PARAMS} -C "${TARGET_BIN}" "${parameters[@]}"
-}
-
-function unzip_file {
-    local file=$1
-    shift
-    local filter=("$@")
-
-    ZIP_ADDITIONAL_PARAMS="-q"
-    if ${TAR_VERBOSE}; then
-        ZIP_ADDITIONAL_PARAMS="-v"
-    fi
-
-    ${SUDO} unzip -o ${ZIP_ADDITIONAL_PARAMS} -d "${TARGET_BIN}" "${file}" "${filter[@]}"
-}
-
-function gunzip_file() {
-    cat | \
-        gunzip
-}
-
-function store_file() {
-    local filename=$1
-    local dirname=$2
-
-    if test -z "${filename}"; then
-        echo "ERROR: File name not specified."
-        return 1
-    fi
-
-    if test -z "${dirname}"; then
-        dirname="${TARGET_BIN}"
-    fi
-
-    >&2 echo "Creating file ${dirname}/${filename}"
-    cat | \
-        ${SUDO} tee "${dirname}/${filename}" >/dev/null
-    echo "${dirname}/${filename}"
-}
-
 function install_file() {
     local filename=$1
     if test -z "${filename}"; then
@@ -274,58 +202,6 @@ function install_completion() {
 function install_manpage() {
     local man=$1
     TARGET="${TARGET_BASE}/share/man/man${man}" install_file "$2" "0644"
-}
-
-# shellcheck disable=SC2120
-function make_executable() {
-    local filename=$1
-
-    if test -z "${filename}"; then
-        cat | while read -r filename2; do
-            >&2 echo "Setting executable bits on ${filename2}"
-            ${SUDO} chmod +x "${filename2}"
-        done
-        return
-    fi
-
-    >&2 echo "Setting executable bits on ${filename}"
-    ${SUDO} chmod +x "${TARGET_BIN}/${filename}"
-}
-
-function rename_file() {
-    local old_name=$1
-    local new_name=$2
-
-    if test -z "${old_name}"; then
-        echo "ERROR: Name of existing file not specified."
-        exit 1
-    fi
-    if test -z "${new_name}"; then
-        echo "ERROR: New name of file not specified."
-        exit 1
-    fi
-
-    >&2 echo "Renaming ${old_name} to ${new_name}..."
-    ${SUDO} mv "${TARGET_BIN}/${old_name}" "${TARGET_BIN}/${new_name}"
-}
-
-function store_completion() {
-    local filename=$1
-
-    if test -z "${filename}"; then
-        echo "ERROR: File name not specified."
-        return 1
-    fi
-
-    ${SUDO} mkdir -p "${TARGET_COMPLETION}"
-
-    cat | \
-        store_file "${filename}.sh" "${TARGET_COMPLETION}"
-    if test -n "${SUDO}"; then
-        ${SUDO} ln -sf "${TARGET_COMPLETION}/${filename}.sh" /etc/bash_completion.d/
-    else
-        >&2 echo "!!! Please make sure the completion if loaded from <${TARGET_COMPLETION}/${filename}.sh>"
-    fi
 }
 
 function install_node_module() {
