22b56f53045290c5dba989e7f218c557d3d176f2
Nicholas Dille <nicholas.dille@mailbox.org>
2021-01-13 17:30:10 +0100

    Closes #837

--- test.sh
+++ test.sh
@@ -10,14 +10,16 @@ if test "${DISTRIB_ID}" != "Ubuntu"; then
     exit 1
 fi
 
+unique_name=$(basename "$(mktemp --dry-run)")
+
 docker ps \
-        --filter name="test-build" \
+        --filter name="test-build-${unique_name}" \
         --all \
         --quiet | \
     xargs -r \
         docker rm --force
 docker run \
-    --name "test-build" \
+    --name "test-build-${unique_name}" \
     --detach \
     --env GITHUB_TOKEN \
     --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
@@ -26,7 +28,7 @@ docker run \
     "nicholasdille/packages-runtime:${DISTRIB_ID,,}-${DISTRIB_CODENAME}"
 
 function cleanup() {
-    docker rm -f "test-build"
+    docker rm -f "test-build-${unique_name}"
 }
 trap cleanup EXIT
 
@@ -42,9 +44,9 @@ while test "$#" -gt 0; do
             packages.json
     )"
     if test "${REQUIRES_DOCKER}" == "true"; then
-        docker exec --env TARGET_BASE "test-build" pkgctl.sh install docker
+        docker exec --env TARGET_BASE "test-build-${unique_name}" pkgctl.sh install docker
     fi
 
-    docker exec --env TARGET_BASE "test-build" pkgctl.sh install "${PACKAGE}"
+    docker exec --env TARGET_BASE "test-build-${unique_name}" pkgctl.sh install "${PACKAGE}"
 
 done
