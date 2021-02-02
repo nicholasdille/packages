0481a00b63a93dc175a0fdbd739fbe7daf006fff
Nicholas Dille <nicholas.dille@mailbox.org>
2021-02-02 15:48:43 +0100

    Fixes after shellcheck

--- scripts/add_from_github.sh
+++ scripts/add_from_github.sh
@@ -15,11 +15,11 @@ if test -z "${GITHUB_TOKEN}" && test "${remaining_rate_limit}" -eq 0; then
     echo "ERROR: GitHub rate limit exceeded and API token not provided."
     exit 1
 fi
-if test -n ${GITHUB_TOKEN}; then
+if test -n "${GITHUB_TOKEN}"; then
     GITHUB_AUTH_PARAM="--header \"Authorization: token ${GITHUB_TOKEN}\""
 fi
 
-json="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}")"
+json="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}")"
 
 name="$(
     echo "${json}" | \
@@ -35,19 +35,19 @@ description="$(
         jq --raw-output '.description'
 )"
 
-releases="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/releases" | jq 'length')"
+releases="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/releases" | jq 'length')"
 if test "${releases}" -gt 0; then
     renovate_source=releases
 
-    has_release="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/releases" | jq 'map(select(.prerelease == false)) | length')"
+    has_release="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/releases" | jq 'map(select(.prerelease == false)) | length')"
     if test "${has_release}" -gt 0; then
-        latest_version="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/releases/latest" | jq --raw-output '.tag_name')"
+        latest_version="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/releases/latest" | jq --raw-output '.tag_name')"
     else
-        latest_version="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/releases" | jq --raw-output '.[0].tag_name')"
+        latest_version="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/releases" | jq --raw-output '.[0].tag_name')"
     fi
 
     assets=$(
-        curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/releases/tags/${latest_version}" | \
+        curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/releases/tags/${latest_version}" | \
             jq --raw-output '.assets[].browser_download_url' | \
             tr '\n' ' ' | \
             sed "s/${latest_version}/\$\{requested_version\}/g" | \
@@ -56,7 +56,7 @@ if test "${releases}" -gt 0; then
 
 else
     renovate_source=tags
-    latest_version="$(curl --silent ${GITHUB_AUTH_PARAM} "https://api.github.com/repos/${repo}/tags" | jq --raw-output '.[0].name')"
+    latest_version="$(curl --silent "${GITHUB_AUTH_PARAM}" "https://api.github.com/repos/${repo}/tags" | jq --raw-output '.[0].name')"
 fi
 
 cat >"${filename}" <<EOF
